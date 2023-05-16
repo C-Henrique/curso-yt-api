38a0d10579ea6399bd1a3f521e7b281035e0a237
Camilo Henrique <80978207+C-Henrique@users.noreply.github.com>
2023-05-16 13:45:33 -0300

    feat: add folders struture

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 import { server } from './server/Server';
-server.listen(process.env.PORT , ()=>{
-    return console.log('Api Okay! Link: http://localhost:'+process.env.PORT);
+server.listen(process.env.PORT ||3333, ()=>{
+    return console.log('Api Okay! Link: http://localhost:'+process.env.PORT || 3333);
 });
